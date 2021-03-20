b25e5e78d8ba49415b34d67466ce34d7d94e1676
ej28power <ej28power@naver.com>
2021-03-20 17:02:24 +0900

    2

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express()
+const path = require('path')
+const createError = require('http-errors')
+
+// Init
+app.listen(3000, () => { console.log('http://127.0.0.1:3000') })
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, './views'))
+app.locals.pretty = true
+
+// Middleware
+
+
+// Router
+app.use('/', express.static(path.join(__dirname, './public')))
+
+const bookRouter = require('./routes/book-router')
+app.use('/book', bookRouter)
+
+
+// Error
+app.use((req, res, next) => {
+	next(createError(404))
+})
+
+app.use((err, req, res, next) => {
+	res.send(err)
+})

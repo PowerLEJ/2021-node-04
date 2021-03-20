1316cd1b2643ce24ed8d4f8bb21ed4e3f38730d4
ej28power <ej28power@naver.com>
2021-03-20 16:19:38 +0900

    1

---
+++ books.sql
@@ -0,0 +1,15 @@
+-- CRUD : Create, Read, Update, Delete
+-- SQL : Structured Query Language
+
+-- Create
+-- INSERT INTO books (bookName, writer, content ) VALUES ('별주부전', '까북이','용왕이 나의 간을...');
+-- INSERT INTO books SET bookName='구운몽전', writer='구운몽', content='나비가 나를 ...';
+
+-- Update
+-- UPDATE books SET bookName="별주세요", writer="거북이" WHERE id=2;
+
+-- Delete
+-- DELETE FROM books WHERE id=3;
+
+-- Read
+-- SELECT * FROM books ORDER BY id DESC; -- ASC는 오름차순

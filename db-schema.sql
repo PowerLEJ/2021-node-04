21cb3a720c701d95175891ed605a95f69225e572
ej28power <ej28power@naver.com>
2021-04-24 17:26:08 +0900

    sql

--- db-schema.sql
+++ db-schema.sql
@@ -12,7 +12,35 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- 테이블 booldook.books 구조 내보내기
+-- 테이블 leepower.api 구조 내보내기
+CREATE TABLE IF NOT EXISTS `api` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `domain` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `appkey` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `userid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+
+-- 테이블 leepower.boards 구조 내보내기
+CREATE TABLE IF NOT EXISTS `boards` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `writer` varchar(255) DEFAULT NULL,
+  `content` text,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `UserId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `UserId` (`UserId`),
+  CONSTRAINT `boards_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `seq-users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+
+-- 테이블 leepower.books 구조 내보내기
 CREATE TABLE IF NOT EXISTS `books` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `bookName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -20,14 +48,14 @@ CREATE TABLE IF NOT EXISTS `books` (
   `content` text COLLATE utf8mb4_unicode_ci,
   `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `uid` int unsigned NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`) USING BTREE,
   KEY `uid` (`uid`),
   CONSTRAINT `FK_books_users` FOREIGN KEY (`uid`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
--- 테이블 booldook.files 구조 내보내기
+-- 테이블 leepower.files 구조 내보내기
 CREATE TABLE IF NOT EXISTS `files` (
   `id` int NOT NULL AUTO_INCREMENT,
   `bookid` int unsigned NOT NULL,
@@ -35,13 +63,28 @@ CREATE TABLE IF NOT EXISTS `files` (
   `savename` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `FK_files_books` (`bookid`),
+  KEY `bookid` (`bookid`),
   CONSTRAINT `FK_files_books` FOREIGN KEY (`bookid`) REFERENCES `books` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
--- 테이블 booldook.sessions 구조 내보내기
+-- 테이블 leepower.seq-users 구조 내보내기
+CREATE TABLE IF NOT EXISTS `seq-users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userid` varchar(24) NOT NULL,
+  `userpw` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `userid` (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+
+-- 테이블 leepower.sessions 구조 내보내기
 CREATE TABLE IF NOT EXISTS `sessions` (
   `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `expires` int unsigned NOT NULL,
@@ -51,16 +94,19 @@ CREATE TABLE IF NOT EXISTS `sessions` (
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
--- 테이블 booldook.users 구조 내보내기
+-- 테이블 leepower.users 구조 내보내기
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `userid` varchar(24) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `userid` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `userpw` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `grade` tinyint NOT NULL DEFAULT '1',
   `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `api` enum('KA','NA','FA','GO','LO') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'LO',
+  `api_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `api_token` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
